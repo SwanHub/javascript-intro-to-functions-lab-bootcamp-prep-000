aff50edb17bdbb8dd7565a1cf989fa4f3be1c45c
Jackson Prince <jdprince555@gmail.com>
2019-03-06 04:31:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,9 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
   return "I can't hear you!"}
 }
+
+function sayHiGrandma(string) {
+  if (string.toUpperCase() === string){
+    return "YES INDEED!"
+  }
+}
